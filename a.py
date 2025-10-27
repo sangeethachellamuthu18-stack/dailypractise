0521d691adcbd03fe17d17042650587a805adfed
sangeethachellamuthu18-stack <sangeethachellamuthu18@gmail.com>
2025-10-27 11:46:50 +0530

    gc

--- a.py
+++ a.py
@@ -1,3 +1,4 @@
+"""
 class destructor:
     def __init__(self):
         print("its a constructor method")
@@ -6,3 +7,9 @@ class destructor:
         print("its a destructor method")
 
 a=destructor()
+"""
+import sys
+x=12
+y="This function returns the size (in bytes) of a Python object."
+print("x", sys.getsizeof(x)) 
+print("y",sys.getsizeof(y))
